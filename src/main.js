851c9ae814d288453d8f3c19f99eb36a51c06f80
andreas.stephan <andreas.stephan@internetx.com>
2019-08-06 15:47:55 +0200

    just a test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
+import SvgIcon from "vue-svgicon";
+import dictionaries from "./translations/index.js";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.prototype.$legends = dictionaries;
+
+Vue.use(SvgIcon, {
+    tagName: "svgicon"
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    store,
+    render: h => h(App)
+}).$mount("#app");
